c2b71b69f4e9bff02108c548e27ddc458c7e8e6c
ricardo-f <riclima1990@gmail.com>
2022-09-10 18:28:58 -0300

    adjusting var declaration and adding description

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,19 @@
 variable "main_cidr" {
-  default = "192.168.0.0/24"
+  default     = "192.168.0.0/24"
+  description = "CIDR block for th VPC"
 }
 
-locals {
-  pvt_sub = "192.168.0.0/25"
-  pub_sub = "192.168.0.128/25"
+variable "pvt_subnet" {
+  default     = "192.168.0.0/25"
+  description = "Subnetting of the VPC CIDR for private subnet"
 }
 
-variable "azs" {
+variable "pub_subnet" {
+  default     = "192.168.0.128/25"
+  description = "Subnetting of the VPC CIDR for public subnet"
+}
+
+variable "az" {
   default = "us-east-1a"
+  description = "AZ for the subnets, since this was a Lab one az was suffice, in prod envs use a minimum of 2 azs"
 }
